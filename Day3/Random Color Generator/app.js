68ca3e800816fe10e4a108dc366a692dd9c45bdb
vrindadixit17 <168673148+vrindadixit17@users.noreply.github.com>
2025-07-03 00:32:00 +0530

    Add files via upload

---
+++ Day3/Random Color Generator/app.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button");
+
+
+btn.addEventListener("click", function () {
+    let h2 = document.querySelector("h2");
+    let randomColor = getRandomColor();
+    h2.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
