3931cc8899e83e164ed13c719231526837e8a0ac
vrindadixit17 <168673148+vrindadixit17@users.noreply.github.com>
2025-07-14 23:19:36 +0530

    Add files via upload

---
+++ Day7/Theme toggle/script.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.getElementById("themeToggleBtn");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    const isDark = document.body.classList.contains("dark-theme");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-theme");
+    }
+});
